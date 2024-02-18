46f3ce7ddf9d37d04e1f68913c908f52545d3339
Sarah-Obarhoro <105218020+Sarah-Obarhoro@users.noreply.github.com>
2024-02-18 20:04:58 +0100

    Completed Navbar styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,95 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { CgMenuRightAlt } from "react-icons/cg";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsSticky(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className="bg-blue align-middle flex justify-between px-8 h-16 pt-4">
-      <h1 className="font-dancingScript text-3xl font-extrabold">Sarah</h1>
-      <ul className="text-navy flex justify-between w-3/5 font-playFair font-medium py-1">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
+    <nav className={`${isSticky ? "sticky top-0 left-0 right-0" : ""}`}>
+      <div className="bg-blue items-center flex justify-between px-8 h-16">
+        <Link to="/">
+          <h1 className="font-dancingScript text-3xl font-extrabold">Sarah</h1>
+        </Link>
 
-        <li>
-          <Link to="/about">About Me</Link>
-        </li>
+        {/* Styling for larger screens */}
+        <ul className="text-navy hidden lg:flex justify-between w-3/5 font-playFair font-medium pl-60">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
 
-        <li>
-          <Link to="/technologies">My Tech Stack</Link>
-        </li>
+          <li>
+            <Link to="/about">About Me</Link>
+          </li>
 
-        <li>
-          <Link to="/projects">My Projects</Link>
-        </li>
+          <li>
+            <Link to="/technologies">My Tech Stack</Link>
+          </li>
 
-        <li>
-          <Link to="/blog">My Blog</Link>
-        </li>
+          <li>
+            <Link to="/projects">My Projects</Link>
+          </li>
 
-        <li>
+          <li>
+            <Link to="/blog">My Blog</Link>
+          </li>
+        </ul>
+
+        <button className="border border-black px-5 py-3 rounded-2xl text-center items-center hover:bg-black hover:text-blue hidden lg:flex font-playFair font-medium">
           <Link to="/contact">Contact Me</Link>
-        </li>
-      </ul>
-      <CgMenuRightAlt className="" />
-    </div>
+        </button>
+
+        {/* Menu button for small screens */}
+        <button onClick={toggleMenu} className="lg:hidden">
+          <CgMenuRightAlt className="text-3xl" />
+        </button>
+      </div>
+
+      {/* Nav items for smaller screens */}
+      {menuOpen && (
+        <div className="mt-2 bg-blue rounded pb-8 mx-4">
+          <ul className="text-navy font-playFair font-medium py-4 pl-5">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+
+            <li>
+              <Link to="/about">About Me</Link>
+            </li>
+
+            <li>
+              <Link to="/technologies">My Tech Stack</Link>
+            </li>
+
+            <li>
+              <Link to="/projects">My Projects</Link>
+            </li>
+
+            <li>
+              <Link to="/blog">My Blog</Link>
+            </li>
+          </ul>
+          <button className="border border-black px-4 py-2 ml-4 rounded-2xl text-center items-center hover:bg-black hover:text-blue font-playFair font-medium text-sm">
+            <Link to="/contact">Contact Me</Link>
+          </button>
+        </div>
+      )}
+    </nav>
   );
 };
 
