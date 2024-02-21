60765f8a1deeb3f12a462180c543571f907298f6
Sarah-Obarhoro <105218020+Sarah-Obarhoro@users.noreply.github.com>
2024-02-21 11:22:07 +0100

    Updated About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,29 @@
+import Image from '../assets/images/Sarah.png'
+
 const About = () => {
   return (
-    <div className="bg-black">
-      <h1 className="text-blue">Hi! I{`'`}m Sarah</h1>
-      <p></p>
+    <div className="bg-black flex">
+      <div>
+        <h1 className="text-blue font-playFair font-bold text-3xl">Hi!</h1>
+        <h2 className="text-blue">I am Sarah</h2>
+        <p className="text-blue">
+          A Software Developer who is passionate about creating not just
+          visually appealing, but highly functional websites and products that
+          prioritize user experience and are easy to navigate. I believe that
+          coding is a superpower, and I take joy in tackling challenges for my
+          clients, solving them one line of code at a time. When I{`'`}m not
+          immersed in code, you{`'`}ll often find me engrossed in fiction, using
+          it as a portal to explore other worlds, or unwinding to the tunes of
+          my favorite music. At other times, I indulge my curiosity by delving
+          into emerging technologies, fascinated by the ever-evolving landscape
+          of innovation. That being said, if you are looking for someone to
+          translate your ideas into tangible digital experiences and transform
+          your concepts into reality, I{`'`}m your girl!
+        </p>
+      </div>
+      <div>
+        <img src={Image} alt="Sarah {`'`}s Image" />
+      </div>
     </div>
   );
 }
